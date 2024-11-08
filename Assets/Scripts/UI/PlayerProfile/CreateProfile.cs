b600bd31c0cfd202845be6921d925b832ea23a56
Alexandre Careau <careau.alexandre@gmail.com>
2024-11-08 15:52:22 -0500

    Added script for profile creation

---
+++ Assets/Scripts/UI/PlayerProfile/CreateProfile.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CreateProfile : MonoBehaviour, IPointerClickHandler
+{
+    private string username;
+    private string password;
+
+    public string Username { get { return username; } }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        setProfileInfo();
+        Debug.Log("Next Menu");
+    }
+
+    private void Awake()
+    {
+    }
+
+    private void setProfileInfo()
+    {
+        int lastChildInd = transform.parent.childCount - 1;
+        Transform parent = transform.parent.GetChild(lastChildInd);
+        string tempPassword = "";
+
+        Debug.Log(parent.name);
+
+        username = parent.GetChild(lastChildInd).GetChild(0).GetChild(0).GetComponent<Text>().ToString();
+        lastChildInd++;
+        tempPassword = parent.GetChild(lastChildInd).GetChild(0).GetChild(0).GetComponent<Text>().ToString();
+        lastChildInd++;
+        if (tempPassword.Equals(parent.GetChild(lastChildInd).GetChild(0).GetChild(0).GetComponent<Text>().ToString()))
+        {
+            password = tempPassword;
+        }
+        Debug.Log(username);
+        Debug.Log(password);
+    }
+}
