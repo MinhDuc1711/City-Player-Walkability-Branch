08709f3c7be3007377057d1a475d45bd1238d2a6
Ryan Sef <74203166+Ryan30012@users.noreply.github.com>
2024-11-08 21:29:32 -0500

    Adding State Script for player states
    Added a script that store the state of the player (walking, crounching, jumping, idle)

---
+++ Assets/Scripts/Movement/State.cs
@@ -0,0 +1,56 @@
+
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class State : MonoBehaviour
+{
+
+    public KeyCode crouch;
+
+    public LayerMask mask;
+
+    private enum state
+    {
+        walking,
+        crouching,
+        jumping,
+        idle
+    }
+
+    private state playerState;
+
+    private Rigidbody player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        player = this.gameObject.GetComponent<Rigidbody>();
+        crouch = KeyCode.LeftControl;
+    }
+
+    private void FixedUpdate()
+    {
+        updateState();
+    }
+
+    private void updateState()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(new Vector3(0, -1, 0)), .1f, mask))
+        {
+            playerState = state.jumping;
+        }
+        else if(player.linearVelocity.x>0 || player.linearVelocity.z > 0)
+        {
+            if (Input.GetKeyDown(crouch))
+            {
+                playerState = state.crouching;
+            }
+            else
+            {
+                playerState = state.walking;
+            }
+        }
+    }
+}
