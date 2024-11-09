1f034147a0af3a8906a9ebca63fdc32e8f66160f
Jason-817 <jasonnovio@hotmail.ca>
2024-11-08 22:39:27 -0500

    Format and Final Prototype
    Implemented drag and drop feature, rotation feature to rotate the object and UI box to move and delete the generated greenery objects. Also formatting the folder to avoid conflicts when merging.

---
+++ Assets/Scripts/GreeneryScripts/GreenObjManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenObjectManager : MonoBehaviour
+{
+    public GameObject[] greeneryObjects; 
+    private Vector3 pos;                  // Position for placing greenery
+    private RaycastHit hit;
+    public GameObject pendingObj;
+
+    [SerializeField] private LayerMask layerMask;
+
+    public bool canPlace = true;
+
+    public float rotateAmount = 45f; // rotation degrees x*45
+
+    private void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask))
+        {
+            pos = hit.point; 
+        }
+    }
+
+    public void SelectObject(int index)
+    {
+        //green object instantiated
+        pendingObj = Instantiate(greeneryObjects[index], pos, transform.rotation);
+    }
+
+    private void Update()
+    {
+        if (pendingObj != null)
+        {
+            pendingObj.transform.position = pos;
+
+            // rotation using R input
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                RotateObject();
+            }
+
+            // left click used to move object
+            if (Input.GetMouseButtonDown(0) && canPlace)
+            {
+                PlaceObject();
+            }
+        }
+    }
+
+    private void RotateObject()
+    {
+        pendingObj.transform.Rotate(Vector3.up, rotateAmount);
+    }
+
+    private void PlaceObject()
+    {
+        pendingObj = null; 
+    }
+}
---
+++ Assets/Scripts/GreeneryScripts/GreenSelection.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+public class GreenSelection : MonoBehaviour
+{
+    public GameObject selectedObj; 
+    public TextMeshProUGUI objNameText; 
+    public GameObject selectUI; 
+
+    public GreenObjectManager greenObjManager; 
+
+    private void Start()
+    {
+        selectUI.SetActive(false); 
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                if (hit.collider.gameObject.CompareTag("Object"))
+                {
+                    Select(hit.collider.gameObject);
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Deselect();
+        }
+    }
+
+    private void Select(GameObject obj)
+    {
+        if (obj == selectedObj) return;
+
+        if (selectedObj != null) Deselect();
+
+        Outline outline = obj.GetComponent<Outline>();
+        if (outline == null) obj.AddComponent<Outline>(); 
+        else outline.enabled = true; 
+
+        objNameText.text = obj.name;
+        selectedObj = obj;
+        selectUI.SetActive(true);
+    }
+
+    private void Deselect()
+    {
+        selectedObj.GetComponent<Outline>().enabled = false;
+        selectUI.SetActive(false) ;
+        selectedObj = null;
+    }
+
+    public void Delete()
+    {
+
+        GameObject objToDestroy = selectedObj;
+        Deselect();
+        Destroy(objToDestroy);
+    }
+
+    public void Move()
+    {
+        greenObjManager.pendingObj = selectedObj;
+    }
+}
