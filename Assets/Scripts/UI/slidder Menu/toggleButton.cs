d2ec993b96a2c7d48c722586f3b4c5517f5e31b6
Ryan Sef <74203166+Ryan30012@users.noreply.github.com>
2024-12-24 14:29:44 -0500

    Added the main script to control showing the slidder's menu
    Integrated mouse click and keybord input to open/hide the UI

---
+++ Assets/Scripts/UI/slidder Menu/toggleButton.cs
@@ -0,0 +1,69 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class toggleButton : UIMenu, IPointerClickHandler
+{
+    public KeyCode keyToOpen = KeyCode.E;
+
+    public uiMenuManager menuManager;
+
+    private Animator SlidderMenu;
+
+    private RectTransform s_RTransform;
+
+    private float open = -271.03f;
+
+    private float close = -930f;
+
+    private bool state = false; //this var is displaying when the UI is shown (true) or hidden (false)
+
+    public float animationTime = 0.35f;
+
+    public override bool State 
+    { 
+        get => state; 
+        set
+        {
+            state = value;
+        }
+    }
+
+    public override float AnimationTime
+    {
+        get => animationTime;
+    }
+
+    private void Awake()
+    {
+        SlidderMenu = GetComponentInParent<Animator>();
+        s_RTransform = SlidderMenu.transform.GetComponent<RectTransform>();
+        menuManager.toggleBtns.Add(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToOpen))
+        {
+            if (!state)
+                menuManager.showUIMenu(this);
+            else
+                menuManager.hideUIMenu(this);
+        }
+    }
+
+    public override void SetActive(bool state) //setting bool to true activate the open animation, false activate the close animation
+    {
+        SlidderMenu.SetBool("SMenu", state);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Should Call the showUI function in the UI manager class
+        if (!state)
+            menuManager.showUIMenu(this);
+        else
+            menuManager.hideUIMenu(this);
+    }
+}
