4c62647a0dd011525bc3aa03d96ffdda4a5e9d9e
Jason-817 <jasonnovio@hotmail.ca>
2025-01-26 11:59:30 -0500

    First test of created green objects
    implemented test where it generates tree and flower prefabs

--- Assets/Tests/GreenerySliderTests.cs
+++ Assets/Tests/GreenerySliderTests.cs
@@ -1,9 +1,8 @@
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.UI;
-using System.Collections.Generic;
+using System.Linq; 
 
-public class GreenerySliderTest
+public class GreenerySliderTests
 {
     private GreeneryGeneration greeneryGeneration;
 
@@ -18,10 +17,34 @@ public class GreenerySliderTest
         greeneryGeneration.LeftGreenStripEnd = new GameObject { transform = { position = new Vector3(10, 0, 0) } };
         greeneryGeneration.RightGreenStripStart = new GameObject { transform = { position = new Vector3(0, 0, 5) } };
         greeneryGeneration.RightGreenStripEnd = new GameObject { transform = { position = new Vector3(10, 0, 5) } };
-        greeneryGeneration.TreePrefabs = new[] { new GameObject(), new GameObject() };
-        greeneryGeneration.FlowerPrefab = new GameObject();
+
+        GameObject treePrefab1 = new GameObject("TreePrefab1");
+        GameObject treePrefab2 = new GameObject("TreePrefab2");
+        greeneryGeneration.TreePrefabs = new[] { treePrefab1, treePrefab2 };
+
+        GameObject flowerPrefab = new GameObject("FlowerPrefab");
+        greeneryGeneration.FlowerPrefab = flowerPrefab;
     }
 
-   
-}
+    [Test]
+    public void GenerateGreenery_DensityVariation_CreatedObjects()
+    {
+        // Act
+        greeneryGeneration.GenerateGreenery(5);
 
+       
+        int treeCount = Object.FindObjectsOfType<GameObject>()
+                              .Count(obj => greeneryGeneration.TreePrefabs.Contains(obj));
+        int flowerCount = Object.FindObjectsOfType<GameObject>()
+                                .Count(obj => obj.name == greeneryGeneration.FlowerPrefab.name);
+
+        Assert.IsTrue(treeCount > 0, "Trees were not generated as expected.");
+        Assert.IsTrue(flowerCount > 0, "Flowers were not generated as expected.");
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.DestroyImmediate(greeneryGeneration.gameObject);
+    }
+}
