18556e6523a12074023278a4b479c22cc45898dc
Jason-817 <jasonnovio@hotmail.ca>
2025-01-26 04:06:40 -0500

    Setup greenery testing
    Set up mock objects and initialize required properties for testing the GreeneryGeneration class.

--- Assets/Tests/GreenerySliderTests.cs
+++ Assets/Tests/GreenerySliderTests.cs
@@ -1,16 +1,27 @@
+using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
-public class GreenerySliderTest : MonoBehaviour
+public class GreenerySliderTest
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private GreeneryGeneration greeneryGeneration;
+
+    [SetUp]
+    public void Setup()
     {
-        
+        GameObject mockGameObject = new GameObject();
+        greeneryGeneration = mockGameObject.AddComponent<GreeneryGeneration>();
+
+        // required properties
+        greeneryGeneration.LeftGreenStripStart = new GameObject { transform = { position = new Vector3(0, 0, 0) } };
+        greeneryGeneration.LeftGreenStripEnd = new GameObject { transform = { position = new Vector3(10, 0, 0) } };
+        greeneryGeneration.RightGreenStripStart = new GameObject { transform = { position = new Vector3(0, 0, 5) } };
+        greeneryGeneration.RightGreenStripEnd = new GameObject { transform = { position = new Vector3(10, 0, 5) } };
+        greeneryGeneration.TreePrefabs = new[] { new GameObject(), new GameObject() };
+        greeneryGeneration.FlowerPrefab = new GameObject();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
+
