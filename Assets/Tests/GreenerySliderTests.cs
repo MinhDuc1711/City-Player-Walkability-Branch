b675804329cfb15a1e3e35251a7a6474d4cb09b0
Jason-817 <jasonnovio@hotmail.ca>
2025-01-26 12:08:05 -0500

    second test on removing greenery objects
    this test creates and removes tree and flower objects

--- Assets/Tests/GreenerySliderTests.cs
+++ Assets/Tests/GreenerySliderTests.cs
@@ -42,9 +42,33 @@ public class GreenerySliderTests
         Assert.IsTrue(flowerCount > 0, "Flowers were not generated as expected.");
     }
 
+    [Test]
+    public void GenerateGreenery_RemovedObjects()
+    {
+        greeneryGeneration.GenerateGreenery(5);
+
+        greeneryGeneration.ClearGreenery();
+
+        int treeCount = GameObject.FindGameObjectsWithTag("Tree").Length;
+        int flowerCount = GameObject.FindGameObjectsWithTag("Flower").Length;
+
+        int objectCount = Object.FindObjectsOfType<GameObject>()
+                                        .Count(obj => greeneryGeneration.TreePrefabs.Contains(obj) ||
+                                                      obj.name == greeneryGeneration.FlowerPrefab.name);
+
+        Assert.AreEqual(0, treeCount, "Trees were not cleared.");
+        Assert.AreEqual(0, flowerCount, "Flowers were not cleared.");
+    }
+
+
     [TearDown]
     public void Teardown()
     {
-        Object.DestroyImmediate(greeneryGeneration.gameObject);
+        foreach (var obj in Object.FindObjectsOfType<GameObject>())
+        {
+            Object.DestroyImmediate(obj);
+        }
     }
 }
+
+
