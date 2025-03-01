074b6215d74ccbbdcaf6ca6aefe693ed43475148
jozesemaan <jozesemaan@gmail.com>
2025-03-01 00:44:28 -0500

    added quixel to get some assests

---
+++ Assets/Scripts/Public Space/BenchGen.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BenchGen : MonoBehaviour
+{
+    public GameObject LeftGreenStripStart;
+    public GameObject LeftGreenStripEnd;
+    public GameObject RightGreenStripStart;
+    public GameObject RightGreenStripEnd;
+
+    public GameObject[] TreePrefabs;
+    public GameObject FlowerPrefab;
+    //public GameObject PotPrefab;
+
+    public Slider greenObjSlider;
+
+    public float ExtraSpacing=3;
+
+    //private GameObject[] greeneryPrefabs; 
+
+    public void Start()
+    {
+        //greeneryPrefabs = new GameObject[] { TreePrefab, FlowerPrefab};
+        greenObjSlider.onValueChanged.AddListener(OnGreenObjectSliderValueChanged);
+        // Initial generation 
+        GenerateGreenery(greenObjSlider.value);
+    }
+
+    void OnGreenObjectSliderValueChanged(float value)
+    {
+        Debug.Log("Green object slider value changed to: " + value);
+        GenerateGreenery(value);
+    }
+
+    public void GenerateGreenery(float density)
+    {
+        if (density != 0)
+        {
+            density = 15 - density; 
+        }
+        ClearGreenery();
+
+        // Rnadom Pattern Generation
+        SpawnGreeneryWithSpacing(LeftGreenStripStart.transform.position, LeftGreenStripEnd.transform.position, density);
+        SpawnGreeneryWithSpacing(RightGreenStripStart.transform.position, RightGreenStripEnd.transform.position, density);
+    }
+
+    void SpawnGreeneryWithSpacing(Vector3 start, Vector3 end, float spacing)
+    {
+        float distance = Vector3.Distance(start, end);
+        int numberOfObjects = Mathf.FloorToInt(distance / spacing);
+        int TreeVariance = 0;
+
+
+        for (int i = 0; i <= numberOfObjects; i++)
+        {
+            float t = (float)i / numberOfObjects; 
+            Vector3 position = Vector3.Lerp(start, end, t);
+            GameObject prefabToSpawn;
+
+
+
+            if (i % 2 ==0)
+            {
+                float randomRotationY = 90 * Random.Range(0, 4); // 0, 90, 180, or 270
+                Quaternion rotation = Quaternion.Euler(0, randomRotationY, 0);
+                Instantiate(TreePrefabs[TreeVariance % 2], position, rotation);
+                TreeVariance++;
+    }
+            else
+            {
+                position.z += (float)0.75;
+                Instantiate(FlowerPrefab, position, Quaternion.identity);
+            }
+
+        }
+    }
+
+    public void ClearGreenery()
+    {
+        foreach (var obj in GameObject.FindGameObjectsWithTag("Tree"))
+        {
+            Destroy(obj);
+        }
+
+        foreach (var obj in GameObject.FindGameObjectsWithTag("Flower"))
+        {
+            Destroy(obj);
+        }
+
+    }
+}
