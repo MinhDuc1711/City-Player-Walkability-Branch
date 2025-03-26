84ab9efa5eb65ad0b5e3cf715ac812434ddb13bc
Jason-817 <jasonnovio@hotmail.ca>
2025-03-26 11:36:53 -0400

    Background Buildings move with enclosure slider
    -Changed Background Buildings to contain left and right parent
    -Background buildings move in accordance to their designated plot: left and right
    -Background Buildings adapted to enclosure slider

--- Assets/Scripts/Connectivity/BackgroundBuildingsMover.cs
+++ Assets/Scripts/Connectivity/BackgroundBuildingsMover.cs
@@ -1,40 +1,56 @@
 using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine.UI;
-using Unity.VisualScripting;
-using System.Linq;
 
 public class BackgroundBuildingsMover : MonoBehaviour
 {
-    private List<float> initialPos = new List<float>();
     public GameObject block;
+    public Transform leftBuildingsGroup;
+    public Transform rightBuildingsGroup;
+
+    private List<float> initialLeftX = new List<float>();
+    private List<float> initialRightX = new List<float>();
 
     private float initialBlockPosX;
 
     private void Start()
     {
+        if (block == null || leftBuildingsGroup == null || rightBuildingsGroup == null)
+        {
+            Debug.LogError("Assign all references (block, left group, right group) in the Inspector!");
+            return;
+        }
+
         initialBlockPosX = block.transform.position.x;
 
-        foreach (Transform child in transform)
-        {
-            initialPos.Add(child.transform.position.x);
-        }
+        foreach (Transform child in leftBuildingsGroup)
+            initialLeftX.Add(child.transform.position.x);
+
+        foreach (Transform child in rightBuildingsGroup)
+            initialRightX.Add(child.transform.position.x);
     }
 
     private void Update()
     {
-        float blockDeltaX = block.transform.position.x - initialBlockPosX;
+        float deltaX = block.transform.position.x - initialBlockPosX;
 
-        int i = 0;
-        foreach (Transform child in transform)
+        for (int i = 0; i < leftBuildingsGroup.childCount; i++)
         {
+            Transform child = leftBuildingsGroup.GetChild(i);
             child.transform.position = new Vector3(
-                initialPos[i] + blockDeltaX,
+                initialLeftX[i] + deltaX, 
                 child.transform.position.y,
                 child.transform.position.z
             );
+        }
 
-            i++;
+        for (int i = 0; i < rightBuildingsGroup.childCount; i++)
+        {
+            Transform child = rightBuildingsGroup.GetChild(i);
+            child.transform.position = new Vector3(
+                initialRightX[i] - deltaX, 
+                child.transform.position.y,
+                child.transform.position.z
+            );
         }
     }
 }
